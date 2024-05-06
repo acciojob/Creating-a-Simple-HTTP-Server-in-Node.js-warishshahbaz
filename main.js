565b937acfdc0ab13d801a25f5d21e041d783135
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-06 18:54:51 +0530

    impl

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const http = require('http');
+const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  // Send "Hello, world!" as the response
+  res.end("Hello, world!\n");
 });
 
 server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
   // TODO: Log a message to the console when the server starts listening on the port
 });
 
 // DO NOT EDIT BELOW THIS LINE
 
-module.exports = { server }
+module.exports = { server };
