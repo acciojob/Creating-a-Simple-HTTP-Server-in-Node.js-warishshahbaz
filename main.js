06e293aa9adc6ba1c43158cf8617ec29f6e3ba40
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-18 12:00:36 +0530

    impl

--- main.js
+++ main.js
@@ -2,27 +2,15 @@ const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log("Request Received");
   // TODO: Send the ""Hello, world!"" response
-
-  if (req.method === "POST") {
-    let body = "";
-    req.on("data", (data) => {
-      body += data.toString();
-    });
-
-    req.on("end", () => {
-      const dateOfBody = JSON.parse(body);
-      console.log(dateOfBody);
-    });
-  }
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  // Send "Hello, world!" as the response
-  res.end("Hello, world!\n");
+  res.write("Hello, world!");
+  res.end();
 });
 
 server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log("starts listening on the port");
 });
 
 // DO NOT EDIT BELOW THIS LINE
